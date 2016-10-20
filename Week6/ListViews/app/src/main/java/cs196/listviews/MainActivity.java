ec04e64f9e508f5ee05a0f5e62e942419305d0eb
tommypacker <tommypacker@gmail.com>
2016-10-19 19:12:24 -0500

    Add week 7 changes

--- Week6/ListViews/app/src/main/java/cs196/listviews/MainActivity.java
+++ Week6/ListViews/app/src/main/java/cs196/listviews/MainActivity.java
@@ -2,24 +2,41 @@ package cs196.listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<User> users = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        final ArrayList<User> users = new ArrayList<>();
         inflateData(users);
 
         UserAdapter adapter = new UserAdapter(getApplicationContext(), users);
 
         ListView userListView = (ListView) findViewById(R.id.userList);
         userListView.setAdapter(adapter);
+
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User elon = users.get(position);
+            }
+        });
     }
 
     public void inflateData(ArrayList<User> users){
@@ -27,5 +44,9 @@ public class MainActivity extends AppCompatActivity {
         users.add(new User("Elon Musk", "Male", 45));
         users.add(new User("Hillary Clinton", "Female", 68));
         users.add(new User("Harambe", "Male Gorilla", 17));
+
+        for(User user: users){
+            mDatabase.child("users").child(user.getName()).setValue(user);
+        }
     }
 }
